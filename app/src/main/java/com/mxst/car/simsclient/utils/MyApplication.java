92c603a4b34da5d5d60e4b7deb2fff45fea7a38d
依然__饭特稀 <707698592@qq.com>
2016-01-28 15:49:15 +0800

    add Bugly

--- app/src/main/java/com/mxst/car/simsclient/utils/MyApplication.java
+++ app/src/main/java/com/mxst/car/simsclient/utils/MyApplication.java
@@ -2,6 +2,7 @@ package com.mxst.car.simsclient.utils;
 
 import android.app.Application;
 
+import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.socialize.PlatformConfig;
 
 import cn.jpush.android.api.JPushInterface;
@@ -14,5 +15,6 @@ public class MyApplication extends Application {
 		JPushInterface.init(this);
 		PlatformConfig.setWeixin("wx97265ee4dec22093", "c4cf7cd23f2b9b4d348314a0df55f4ea");
 		PlatformConfig.setQQZone("100371282", "aed9b0303e3ed1e27bae87c33761161d");
+		CrashReport.initCrashReport(this, "900019082", false);
 	}
 }
